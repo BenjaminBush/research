f5c7b8811e77559ac0dde8fe2b7ef8b01d88b978
Benjamin Bush <ben.bush4@gmail.com>
2018-07-28 11:15:05 -0400

    before using new dataset

--- TrafficLSTM/generator.py
+++
@@ -1,9 +0,0 @@
-'''
-Class that creates a custom Keras Data Generator
-'''
-# import numpy as np
-# class DataGenerator(object):
-# 	def __init__(self, name):
-# 		self.name = name
-# 		self.batch_size = 128
-# 		self.
