ef67e9b1be2391fac8c4ca5a04521076811d3f04
Benjamin Bush <ben.bush4@gmail.com>
2018-10-22 10:12:38 -0500

    github 2fa sucks

--- scripts/consume.py
+++ scripts/consume.py
@@ -1,5 +1,7 @@
 import gnsq
 import rdtscp_module
+import numpy as np
+import sys
 
 # Variable Initialization
 global true_flows
@@ -15,9 +17,9 @@ messages_processed = 0
 # NSQ Setup
 topic = 'nsq-spark-out'
 channel = 'asdf'
-address = 'localhost'
+address = '192.168.0.120'
 port = '4150'
-reader = gnsq.Reader(topic, channel, address+':'+port)
+reader = gnsq.Reader(topic, channel, address+':'+port, max_in_flight=1000, lookupd_poll_interval=1)
 
 @reader.on_message.connect
 def handler(reader, message):
@@ -28,6 +30,7 @@ def handler(reader, message):
     global predicted_flows
     global latencies
     global messages_processed
+    #print(message.body.decode("utf-8"))
 
     data = message.body.decode("utf-8").split(";")
     
@@ -35,12 +38,22 @@ def handler(reader, message):
     true_flows.append(data[1])
     old_ns = int(float(data[2]))
 
+    #print("Messages {} initiated at time {} and received at time {}".format(messages_processed, old_ns, curr_ns))
+    #print(messages_processed)
+
     latency = curr_ns - old_ns
     latencies.append(latency)
 
-    print("Latency is {} ns".format(latency))
+    # print("Latency is {} ns".format(latency))
+
     messages_processed += 1
 
+    if messages_processed % 200 == 0:
+        print(messages_processed)
 
-
+    if messages_processed % 20000 == 0:
+        np.savetxt('latency.txt', np.asarray(latencies), fmt='%d')
+        #np.savetxt('predicted.txt', np.asarray(predicted_flows), fmt='%d')
+        #np.savetxt('true.txt', np.asarray(true_flows), fmt='%d')
+        sys.exit(0)
 reader.start()
--- scripts/publish.py
+++ scripts/publish.py
@@ -3,21 +3,25 @@ import pandas as pd
 import numpy as np
 import rdtscp_module
 import time
+import random
 
 # Variable initialization
 test = '../data/pems_test.csv'
 df = pd.read_csv(test, encoding='utf-8').fillna(0)
 df = np.array(df)
-length = df.shape[0]
+#length = df.shape[0]
+length = 2020
 
 index = 0
 lag = 12
 
 # NSQ Setup
 topic = 'nsq-spark-in'
-conn = gnsq.Nsqd(address='127.0.0.1', http_port='4151')
+conn = gnsq.Nsqd(address='192.168.0.120', http_port='4151')
 
-while index < length:
+num_batches = 0
+
+while num_batches < 100:
     # Collect flows from dataframe
     data = df[index:index+lag]
     flows = data[:,1]
@@ -34,9 +38,17 @@ while index < length:
 
     # Increment index
     index += 1
+    if index >= 2000:
+        index = 0
+
+    # Check for a new batch, potentially sleep
+    if index % 200 == 0:
+        num_batches += 1
+        sleep_time = random.uniform(1.5, 3)
+        time.sleep(sleep_time)
 
     # Publish and sleep
     response = conn.publish(topic, message)
     if response == b'OK':
         print(index)
-    time.sleep(1)
+    #time.sleep(0.01)
