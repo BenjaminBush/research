39a4acd7eecc88517dd4cbe25df0043a7b91105d
Benjamin Bush <ben.bush4@gmail.com>
2018-11-06 15:15:47 -0600

    refactored peridic producer to a class... not sure if multiprocessing is working yet

--- kafka_scripts/periodic_producer.py
+++ kafka_scripts/periodic_producer.py
@@ -5,60 +5,66 @@ import numpy as np
 import rdtscp_module
 import time
 import random
+import re
 
-# Variable initialization
-test = '../data/pems_test.csv'
-df = pd.read_csv(test, encoding='utf-8').fillna(0)
-df = np.array(df)
+class PeriodicProducer(object):
+    def __init__(self, test_file, max_messages=20000, random_sleep=0, bootstrap_servers='localhost:9092'):
+        self.test_file = test_file
+        self.max_messages = max_messages
+        self.random_sleep = random_sleep
 
-length = 2020
-index = 0
-lag = 12
+        self.df = pd.read_csv(self.test_file, encoding='utf-8').fillna(0)
+        self.df = np.array(self.df)
+
+        self.length = 2000
+        self.index = 0
+        self.lag = 12
+        self.messages_sent = 0
+
+        self.pattern = re.compile(r"../data/(?P<city>[a-zA-Z]+?)/(?P<test_file>[a-zA-Z0-9 ]+?).csv")
+        self.m = self.pattern.search(self.test_file)
+        self.city = self.m.group('city')
+
+        # Kafka setup
+        self.producer = KafkaProducer(bootstrap_servers=[bootstrap_servers])
+        self.topic = str(self.city) + "-input"    
 
 
-# Kafka setup
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-topic = 'input'
+        self.sleep_times = []
+        if self.random_sleep:
+            self.sleep_times = np.loadtxt('../sleep/random_sleep_times.txt').tolist()
+        else:
+            self.sleep_times = np.loadtxt('../sleep/uniform_sleep_times.txt').tolist()
 
-# Experimental values
-messages_sent = 0
-max_messages = 20000
+    def run(self):
+        while self.messages_sent < self.max_messages:
+            # Collect flows from dataframe
+            data = self.df[self.index:self.index+self.lag]
+            flows = data[:,1]
 
-random_sleep = 0        # Binary flag for which sleep list to loat
-sleep_times = []
-if random_sleep:
-    sleep_times = np.loadtxt('../sleep/random_sleep_times.txt').tolist()
-else:
-    sleep_times = np.loadtxt('../sleep/uniform_sleep_times.txt').tolist()
+            # Create the message
+            message = ';'.join(str(flow) for flow in flows)
 
-while messages_sent < max_messages:
-    # Collect flows from dataframe
-    data = df[index:index+lag]
-    flows = data[:,1]
+            fake_prediction = ";" + str(0)
+            message += fake_prediction
 
-    # Create the message
-    message = ';'.join(str(flow) for flow in flows)
+            # Timestamp for latency
+            ns = rdtscp_module.rdtscp()
 
-    fake_prediction = ";" + str(0)
-    message += fake_prediction
+            # Add timestamp to message
+            ns_data = ';'+str(ns)
+            message += ns_data
 
-    # Timestamp for latency
-    ns = rdtscp_module.rdtscp()
+            self.producer.send(self.topic, value=str.encode(message))
+            self.producer.flush()
 
-    # Add timestamp to message
-    ns_data = ';'+str(ns)
-    message += ns_data
-
-    producer.send(topic, value=str.encode(message))
-    producer.flush()
-
-    # Introduce periodic behavior by sleeping  after producing message
-    time.sleep(sleep_times[index])
-    
-    # Increment index
-    index += 1
-    if index >= 2000:
-        index = 0
+            # Introduce periodic behavior by sleeping  after producing message
+            time.sleep(self.sleep_times[self.index])
+            
+            # Increment index
+            self.index += 1
+            if self.index >= 2000:
+                self.index = 0
 
 
 
---
+++ kafka_scripts/run_producers.py
@@ -0,0 +1,24 @@
+import os
+import multiprocessing
+from periodic_producer import PeriodicProducer
+
+
+def create_and_run(city):
+	test_file = '../data/' + str(city) + '/test.csv'
+	producer = PeriodicProducer(test_file)
+	producer.run()
+
+
+
+
+
+if __name__ == "__main__":
+	process_idx = 0
+	#cities = ['athens', 'east_rancho_dominguez', 'el_segundo', 'long_beach', 'lynwood', 'norwalk', 'redondo', 'wilmington']
+	cities = ['redondo', 'el_segundo']
+	for city in cities:
+		p = multiprocessing.Process(target=create_and_run(city))
+		os.system("taskset -p -c %d %d" % (process_idx % multiprocessing.cpu_count(), os.getpid()))
+		process_idx += 1
+		p.start()
+
